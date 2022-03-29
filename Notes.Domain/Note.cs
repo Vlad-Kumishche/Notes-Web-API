ab1145eeaf154fe0e0f4c05a318678167af447f8
Uladzislau Kumishcha <mottedli@ya.ru>
2022-03-29 12:45:55 +0300

    'Note' class implemented.

--- Notes.Domain/Note.cs
+++ Notes.Domain/Note.cs
@@ -2,6 +2,11 @@
 {
     public class Note
     {
-
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public string? Details { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime EditDate { get; set; }
     }
 }
