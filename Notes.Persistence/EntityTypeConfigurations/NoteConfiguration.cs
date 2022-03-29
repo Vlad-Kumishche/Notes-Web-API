77389bf3b01ec4101a944bbc10b1654acf1ce5e8
Uladzislau Kumishcha <mottedli@ya.ru>
2022-03-29 13:30:46 +0300

    'NoteConfiguration' implemented.

--- Notes.Persistence/EntityTypeConfigurations/NoteConfiguration.cs
+++ Notes.Persistence/EntityTypeConfigurations/NoteConfiguration.cs
@@ -1,6 +1,16 @@
-﻿namespace Notes.Persistence.EntityTypeConfigurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notes.Domain;
+
+namespace Notes.Persistence.EntityTypeConfigurations
 {
-    public class NoteConfiguration
+    public class NoteConfiguration : IEntityTypeConfiguration<Note>
     {
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            builder.HasKey(note => note.Id);
+            builder.HasIndex(note => note.Id).IsUnique();
+
+        }
     }
 }
